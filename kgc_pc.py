9ce806821b6fb429cfcc05d18c808c4987d06d6c
krysar <fpolocek@krysicka.cz>
2025-06-03 16:08:43 +0200

    Fixed python program crashing when game scene isn't flight, code cleanup

--- kgc_pc.py
+++ kgc_pc.py
@@ -12,20 +12,11 @@ krpc_conn = krpc.connect(name='KGC 0.02')
 if ser_conn.is_open == False:
     ser_conn.open()
 
-vessel = krpc_conn.space_center.active_vessel # type: ignore
-
-# flight = krpc_conn.add_stream(vessel.flight)
-
-print(vessel.name)
-
 tst = 1024
 verb = 0
 noun = 0
 
 while True:
-#    print("Altitude: ", flight().surface_altitude, flush=True)
-#    print("Periapsis: ", vessel.orbit.periapsis, flush=True)
-#    print("Waiting bytes: ", ser_conn.in_waiting)
     ser_in = ser_conn.readline()
     ser_str = ser_in.decode("utf-8", "strict")
     ser_in_str = ser_str.strip()
@@ -40,20 +31,23 @@ while True:
         elif i.startswith("N:"):
             noun = i.split(':')
             noun = noun[1].strip()
-            noun = int(verb)
-        print("Verb is ", verb, ", noun is ", verb)
+            noun = int(noun)
+        print("Verb is ", verb, ", noun is ", noun)
 
-    match verb:
-        # Verb 1 = Display flight info
-        case 1:
-            match noun:
-                # Noun 1 = Altitude + Apoapsis + Periapsis
-                case 1:
-                    ser_out_str = str(int(vessel.flight().surface_altitude)) + ";" + str(int(vessel.orbit.apoapsis_altitude)) + ";" + str(int(vessel.orbit.periapsis_altitude))
-                    ser_out = ser_out_str.encode("utf-8")
-                    ser_conn.write(ser_out)
-        case 2:
-            ser_out_str = str(tst) + "\n"
-            ser_out = ser_out_str.encode("utf-8")
-            ser_conn.write(ser_out)
-            tst += 1
+    if str(krpc_conn.krpc.current_game_scene) == "GameScene.flight": # type: ignore
+        vessel = krpc_conn.space_center.active_vessel # type: ignore
+        match verb:
+            # Verb 1 = Display flight info
+            case 1:
+                match noun:
+                    # Noun 1 = Altitude above sea level + Apoapsis + Periapsis
+                    case 1:
+                        ser_out_str = str(int(vessel.flight().mean_altitude)) + ";" + str(int(vessel.orbit.apoapsis_altitude)) + ";" + str(int(vessel.orbit.periapsis_altitude))
+                        ser_out = ser_out_str.encode("utf-8")
+                        ser_conn.write(ser_out)
+            # Testing output
+            case 2:
+                ser_out_str = str(tst) + "\n"
+                ser_out = ser_out_str.encode("utf-8")
+                ser_conn.write(ser_out)
+                tst += 1
